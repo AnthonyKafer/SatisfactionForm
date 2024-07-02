e08b076109ab5a41e237f8cba0a291510d03164e
Anthony <anthony.vieira.lino@gmail.com>
2024-07-02 19:00:39 -0300

    last of 2 jul

--- src/components/DefaultBtn.jsx
+++ src/components/DefaultBtn.jsx
@@ -1,9 +1,9 @@
 import "./DefaultBtn.css"
 
 
-export function DefaultBtn ({btnType, text, icon}){
+export function DefaultBtn ({btnType, text, className, icon}){
 
     return(
-        <button type={btnType}>{text} {icon}</button>
+        <button type={btnType} className={className}>{text} {icon}</button>
     )
 } 
